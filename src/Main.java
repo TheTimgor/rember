00ecff0bdb152abf6f8f2e2e8dac79c3b0fa2c77
timgor <egor.vladone@gmail.com>
2019-11-01 12:31:50 -0400

    closing makes it faster

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +13,7 @@ public class Main {
 
     private static double width;
     private static double height;
+    private static double duration = 1000;
 
     public static void main(String[] args) throws InterruptedException {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -18,7 +21,7 @@ public class Main {
         height = screenSize.getHeight();
         while (true){
             drawNew();
-            Thread.sleep((long)(Math.random()*1000));
+            Thread.sleep((long)(Math.random()*duration));
         }
     }
 
@@ -29,6 +32,13 @@ public class Main {
             frame.setSize(300,300);
             frame.setLocation((int)(Math.random()*(width-300)),(int)(Math.random()*(height-300)));
             frame.setVisible(true);
+            WindowListener exitListener = new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    duration *= 0.5;
+                }
+            };
+            frame.addWindowListener(exitListener);
             BufferedImage image;
             image = ImageIO.read(imin);
             frame.getGraphics().drawImage(image,0,0,300, 300, frame.getContentPane());
@@ -37,14 +47,13 @@ public class Main {
                 public void run() {
                     try {
                         Thread.sleep((long)(Math.random()*10000));
-                        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+                        frame.dispose();
                     } catch (InterruptedException e) {
-                        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+                        frame.dispose();
                         e.printStackTrace();
                     }
                 }
             });
-
             thread.start();
         } catch (IOException e) {
             e.printStackTrace();
