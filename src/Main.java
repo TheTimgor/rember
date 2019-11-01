9e9fcf2b1d141ff0fabbffb5b3c741b54ff1a4f1
timgor <egor.vladone@gmail.com>
2019-11-01 13:54:01 -0400

    angery

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,8 +16,11 @@ public class Main {
     private static double width;
     private static double height;
     private static double duration = 1000;
+    private static int closes = 0;
+//    private static BufferedImage image;
+//    private static InputStream imin;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         width = screenSize.getWidth();
         height = screenSize.getHeight();
@@ -25,38 +30,40 @@ public class Main {
         }
     }
 
-    private static void drawNew(){
-        try {
-            InputStream imin = Main.class.getResourceAsStream("/rember.png");
-            JFrame frame = new JFrame("Error");
-            frame.setSize(300,300);
-            frame.setLocation((int)(Math.random()*(width-300)),(int)(Math.random()*(height-300)));
-            frame.setVisible(true);
-            WindowListener exitListener = new WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    duration *= 0.5;
-                }
-            };
-            frame.addWindowListener(exitListener);
-            BufferedImage image;
-            image = ImageIO.read(imin);
-            frame.getGraphics().drawImage(image,0,0,300, 300, frame.getContentPane());
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep((long)(Math.random()*10000));
-                        frame.dispose();
-                    } catch (InterruptedException e) {
-                        frame.dispose();
-                        e.printStackTrace();
-                    }
-                }
-            });
-            thread.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static void drawNew() throws IOException {
+        InputStream imin = Main.class.getResourceAsStream("/rember.png");
+
+        JFrame frame = new JFrame("Error");
+        frame.setSize(300,300);
+        frame.setLocation((int)(Math.random()*(width-300)),(int)(Math.random()*(height-300)));
+        frame.setVisible(true);
+        WindowListener exitListener = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                duration *= 0.5;
+                closes ++;
+            }
+        };
+        frame.addWindowListener(exitListener);
+        BufferedImage image = ImageIO.read(imin);
+        Graphics g = frame.getGraphics();
+        g.drawImage(image,0,0,300, 300, frame.getContentPane());
+        g.setColor(new Color(0x11ff0000,true));
+        for(int i=0; i < closes; i++){
+            g.fillRect(0,0,300,300);
         }
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep((long)(Math.random()*10000));
+                    frame.dispose();
+                } catch (InterruptedException e) {
+                    frame.dispose();
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
